f686ee26a823cf62e9d5de5c8ff6f3aae844a449
lyq12345 <971484814@qq.com>
2022-05-16 01:09:40 -0700

    first-commit

--- src/main/java/com/team8/cs223project/Cs223ProjectApplication.java
+++ src/main/java/com/team8/cs223project/Cs223ProjectApplication.java
@@ -3,9 +3,11 @@ package com.team8.cs223project;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("com.team8.cs223project.mapper")
+@EnableTransactionManagement
 public class Cs223ProjectApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/team8/cs223project/controller/TransactionController.java
@@ -0,0 +1,9 @@
+package com.team8.cs223project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/transaction")
+public class TransactionController {
+}
